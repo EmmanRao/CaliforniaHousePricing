294772ef2015a3a9d3eb492cb49aaca1dd908b82
EmmanRao <emman.duaa143@gmail.com>
2023-02-18 17:24:00 +0500

    this commit is for web application

--- app.py
+++ app.py
@@ -15,17 +15,14 @@ def home():
 '''path'''
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
-    data=request.get_json('data')
+    data=request.json['data']
     print(data)
-    print(np.array(list(data.value())).reshape(1,-1))
+    print(np.array(list(data.values())).reshape(1,-1))
     new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
     output=model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
 
-@app.route('/home')
-def home1():
-    return ('my cute boyfriend is hell cheesy thats why I is dramebaz and emotional blackmailer else Im innocent and its important to be a blackmailer aab kaam bhi to nikalwana na')
 
 if __name__=="__main__":
     app.run(debug=True)
